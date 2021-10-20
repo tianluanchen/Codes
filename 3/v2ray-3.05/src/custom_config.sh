942ad8eb2dcc69249425faec17abd451935c8357
ayouth <zxl-life@qq.com>
2021-10-20 18:22:25 +0800

    update the repository

--- 3/v2ray-3.05/src/custom_config.sh
+++
@@ -1,6 +0,0 @@
-# custom config
-if [[ -f /etc/v2ray/custom/config.json ]]; then
-	custom_config_file="/etc/v2ray/custom/config.json"
-	sed -i "/\/\/include_config/r $custom_config_file" $v2ray_server_config
-	sed -i "s#//include_config#,#" $v2ray_server_config
-fi
